5192fc29f0b8bec0df2cff05c3d2678625cbaaa9
Roman <romanmutel@gmail.com>
2022-02-18 14:50:10 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from itertools import combinations, groupby
+
+'''
+Discrete math laboratory work #01
+Done by Roman Mutel and Marko Ruzak
+'''
